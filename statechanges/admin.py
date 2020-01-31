578706d06809b2ab63c1c2415e092445bb48d6cd
Deofex <roy@powerplatz.nl>
2020-01-31 09:49:02 +0000

    Changed and add models

--- statechanges/admin.py
+++ statechanges/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from .models import Block
 from .models import StateChange
 from .models import CryptoPrice
 from .models import BurnTransaction
 
+
 # Register your models here.
+admin.site.register(Block)
 admin.site.register(StateChange)
 admin.site.register(CryptoPrice)
 admin.site.register(BurnTransaction)
--- statechanges/models.py
+++ statechanges/models.py
@@ -1,33 +1,55 @@
 from django.db import models
 from django.utils import timezone
 
+class Block(models.Model):
+    '''Information about block'''
+    blocknumber = models.IntegerField(primary_key=True)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.blocknumber)
+
 # Create your models here.
 class StateChange(models.Model):
     '''Summery of GET state changes which are posted hourly on the blockchain'''
-    date = models.DateTimeField()
-    blocknumber = models.IntegerField()
-    sumstatechanges = models.IntegerField()
-    firings0count = models.IntegerField()
-    firings1count = models.IntegerField()
-    firings2count = models.IntegerField()
-    firings3count = models.IntegerField()
-    firings4count = models.IntegerField()
-    firings5count = models.IntegerField()
-    firings6count = models.IntegerField()
-    firings7count = models.IntegerField()
-    firings8count = models.IntegerField()
-    firings9count = models.IntegerField()
-    firings10count = models.IntegerField()
-    firings11count = models.IntegerField()
-    firings12count = models.IntegerField()
-    firings13count = models.IntegerField()
-    wiringscount = models.IntegerField()
-    unknownscount = models.IntegerField()
-
+    StateChangeTypes = (
+        ('f0', 'Ticket created'),
+        ('f1', 'Ticket blocked'),
+        ('f2', 'Ticket sold on primary market'),
+        ('f3', 'Ticket sold on secondary market'),
+        ('f4', 'Ticket bought back by organizer'),
+        ('f5', 'Ticket cancelled'),
+        ('f6', 'Ticket put for sale'),
+        ('f7', 'Show cancelled'),
+        ('f8', 'Ticket not resold'),
+        ('f9', 'Ticket not sold on primary market'),
+        ('f10', 'Ticket sold on the secondary market'),
+        ('f11', 'Ticket scanned'),
+        ('f12', 'Show over'),
+        ('f13', 'Ticket unblocked'),
+        ('w', 'New wiring'),
+    )
+    hash = models.CharField(
+        max_length=100,
+        primary_key=True,
+    )
+    previoushash = models.CharField(
+        max_length=100,
+    )
+    statechangetype = models.CharField(
+        max_length =3,
+        choices = StateChangeTypes
+    )
+    block = models.ForeignKey(
+        Block,
+        on_delete=models.CASCADE
+    )
 
 class CryptoPrice(models.Model):
     '''The price of a cryptotoken in different valuta'''
-    name = models.TextField()
+    name = models.CharField(
+        max_length=3
+    )
     price_eur = models.FloatField()
 
     def updateeurprice(self,price):
