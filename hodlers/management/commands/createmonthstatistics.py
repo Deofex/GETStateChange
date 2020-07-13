91264b5df5c6ecca51db46346ae2921aece69070
Deofex <roy@powerplatz.nl>
2020-07-13 14:42:19 +0200

    Add and removed statistics in monthly summary

--- hodlers/management/commands/createmonthstatistics.py
+++ hodlers/management/commands/createmonthstatistics.py
@@ -1,48 +1,45 @@
 from collections import defaultdict
 from decimal import Decimal
 from django.core.management.base import BaseCommand
-from hodlers.models import GETTransaction
-import numpy
+from hodlers.models import GETTransaction, GETPeriodSummary
 
 def create_monthstats(balances):
     # Zero out the holding of the addresses which shouldn't be part of the
     # rapport (SF address + burn address)
-    excludeaddress = ["0x06d191c4bc47138d5d79eb881deea86c93e6603b",
-        "0x08e41b9bd40a4e1d312379fdced8dac07665488c",
-        "0x2408c65b811a4b51e426795d4f855b7b596b04d2",
-        "0x0000000000000000000000000000000000000000",
-        "0x2752cd55caf736de9da26e555f602cafecd81282",
-        "0x36842c31d6e82d72139b42efc35f3d37de36da5e" ]
-
+    excludeaddress = ["0x0000000000000000000000000000000000000000",
+    "0x2408c65b811a4b51e426795d4f855b7b596b04d2",
+    "0x08e41b9bd40a4e1d312379fdced8dac07665488c"
+    ]
     for ea in excludeaddress:
         balances[ea] = 0
 
-    # Create a list from the hashtable and remove the addresses with barely
-    # any tokens
-    bottom_limit = Decimal("1")
+    # Store the amount of tokens which are stored on Exchanges
+    etherdeltabalance = balances["0x8d12a197cb00d4747a1fe03395095ce2a5cc6819"]
+    coinonebalance = balances["0x167a9333bf582556f35bd4d16a7e80e191aa6476"]
+    idexbalance = balances["0x2a0c0dbecc7e4d658f48e01e3fa353f44050c208 "]
+    liquidbalance = balances["0xedbb72e6b3cf66a792bff7faac5ea769fe810517"] + \
+        balances["0xdf4b6fb700c428476bd3c02e6fa83e110741145b"]
+    hotbitbalance = balances["0x274f3c32c90517975e29dfc209a23f315c1e5fc7"]
+    uniswapbalance = balances["0x2680a95fc9de215f1034f073185cc1f2a28b4107 "]
+    totalexchangebalance = etherdeltabalance + coinonebalance + idexbalance + \
+        liquidbalance + hotbitbalance + uniswapbalance
+
+    # Create a list from the hashtable and remove the addresses with less
+    # than 5 tokens
+    bottom_limit = Decimal("5")
     balances = {k: balances[k] for k in balances if balances[k] > bottom_limit}
     balances = [{"address": a, "amount": b} for a, b in balances.items()]
     balances = sorted(balances, key=lambda b: -abs(b["amount"]))
 
-    # Create a list with top 1000 addresses
-    alladdresses = [balance['address'] for balance in balances]
-    allvalues = [balance['amount'] for balance in balances]
-
-    print(sum(allvalues))
-
     output = {
-        'activeaddresses':len(alladdresses),
-        'top100addresses':alladdresses[0:100],
-        'top500addresses':alladdresses[0:500],
-        'top1000addresses':alladdresses[0:1000],
-        'mean':numpy.mean(allvalues),
-        'top100valuesmean':numpy.mean(allvalues[0:100]),
-        'top500valuesmean':numpy.mean(allvalues[0:500]),
-        'top1000valuesmean':numpy.mean(allvalues[0:1000]),
-        'median':numpy.median(allvalues),
-        'top100valuesmedian':numpy.median(allvalues[0:100]),
-        'top500valuesmedian':numpy.median(allvalues[0:500]),
-        'top1000valuesmedian':numpy.median(allvalues[0:1000]),
+        'activeaddresses':len(balances),
+        'etherdeltabalance':coinonebalance,
+        'coinonebalance':coinonebalance,
+        'idexbalance' :idexbalance,
+        'liquidbalance':liquidbalance,
+        'hotbitbalance':hotbitbalance,
+        'uniswapbalance':uniswapbalance,
+        'totalexchangebalance':totalexchangebalance
     }
 
     return(output)
@@ -54,37 +51,50 @@ def collect_monthinfo():
 
     # Create Default dictionary, with a decimal number (0)
     balances = defaultdict(Decimal)
+
+    # Create the burn address and add the amount of GET minted in it
     burnaddress = "0x0000000000000000000000000000000000000000"
     balances[burnaddress] = Decimal("33368773.400000170376363909")
-    for trans in GETTransaction.objects.all():
+
+    # Set the initial counters used to track the amount of transactions in a
+    # period + the amount of GET involved in it
+    transactioncount = 0
+    transactionamountcount = 0
+
+    for trans in GETTransaction.objects.order_by('block'):
         # If the month change, we have to collect information from the previous
         # month.
         if trans.block.date.month != month:
-            # Create period name
+            # Create period name from the last collected period
             periodname = ("{}-{}".format(month,year))
 
             # Get the stats from the month
             periodinfo = create_monthstats(balances)
-            #TEMP------------->
-            #for i in periodinfo:
-            #    print ("{} - {}".format(i["address"], i["amount"]))
-            #
-            print ("------------{}---------------".format(periodname))
-            print("Active addresses (more than 1 GET stored): {}".format(
+
+            # Print information about this stats
+            print("------------{}---------------".format(periodname))
+            print('Amount of transactions which found place: {}'.format(
+                transactioncount
+            ))
+            print('Amount of GET transfered between wallets: {}'.format(
+                transactionamountcount))
+            print("Active addresses (more than 5 GET stored): {}".format(
                 periodinfo["activeaddresses"]))
-            print("Mean: {}".format(periodinfo["mean"]))
-            print("Mean top 100: {}".format(periodinfo["top100valuesmean"]))
-            print("Mean top 500: {}".format(periodinfo["top500valuesmean"]))
-            print("Mean top 1000: {}".format(periodinfo["top1000valuesmean"]))
-            print("Median: {}".format(periodinfo["median"]))
-            print("Median top 100: {}".format(periodinfo["top100valuesmedian"]))
-            print("Median top 500: {}".format(periodinfo["top500valuesmedian"]))
-            print("Median top 1000: {}".format(periodinfo["top1000valuesmedian"]))
-            #<---------ENDTEMP
+            print('Amount of GET stored on exchanges: {}'.format(
+                periodinfo["totalexchangebalance"]
+            ))
 
             # Set the month and year to the new block
             month = trans.block.date.month
             year = trans.block.date.year
+
+            #Reset transactionCounts
+            transactioncount = 0
+            transactionamountcount = 0
+
+        # Add 1 transaction to the counter
+        transactioncount += 1
+        transactionamountcount += + trans.amount
 
         # Add transaction amount to the TO address
         balances[trans.toaddress.address] = \
@@ -93,6 +103,7 @@ def collect_monthinfo():
         balances[trans.fromaddress.address] = \
             balances[trans.fromaddress.address] - trans.amount
 
+    periodinfo = create_monthstats(balances)
 
 # Base class, the start of each Django management command
 class Command(BaseCommand):
@@ -100,3 +111,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         collect_monthinfo()
+
+
+#Monthinfo:
+# +Amount of wallets with 5+ GET in it
+# +Amount of transactions
+# +Amount of GET transfered from 1 wallet to another
+# +Amount of GET on the exchanges
+# -On page (whale/guppies/dutchies etc.)
